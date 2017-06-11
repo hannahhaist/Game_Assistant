d4fc0fb265d7351a6273972a045d21a19625669e
YekaterinaS <yekaterina.shneider@student.reutlingen-university.de>
2017-06-11 12:14:53 +0200

    -settings layout files mostly done -assistant object and the settings logic developed -todo: complete the settings logic fot table, timer, player number and player names settings (in progress)

--- app/src/main/java/com/moco/android/assistant/DiceSettingsActivity.java
+++ app/src/main/java/com/moco/android/assistant/DiceSettingsActivity.java
@@ -3,18 +3,82 @@ package com.moco.android.assistant;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DiceSettingsActivity extends Activity {
+
+    Assistant assistant = null;
+    //spinner arrays
+    private List<String> types = new ArrayList<>(Arrays.asList("cube", "tetrahedron", "octahedron"));
+    private List<String> number =  new ArrayList<>(Arrays.asList("1", "2", "3"));
+    Spinner sprType;
+    Spinner sprNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dice_settings);
+        //initialize spinner
+        sprType = (Spinner)findViewById(R.id.sprDiceType);
+        sprNumber = (Spinner)findViewById(R.id.sprDiceNumber);
+        fillSpinner(types, sprType);
+        fillSpinner(number, sprNumber);
+        //get data from Intent
+        Intent i = getIntent();
+        assistant = (Assistant) i.getSerializableExtra("assistant");
     }
 
-    //called when user taps on continue button. forwards to table settings
-    protected void toTableSettings(View view){
-        Intent intent = new Intent(this, TableSettingsActivity.class);
-        startActivity(intent);
+    private void fillSpinner(List<String> spinnerArray, Spinner spr){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        Spinner sItems = (Spinner) findViewById(R.id.spinner1);
+        spr.setAdapter(adapter);
+    }
+
+    //called when user taps on continue button. writes dice settings into the hashmap, forwards to next settins (if there are other features)
+    protected void toNextSettings(View view){
+        Map<String, Map<String, String>> features = assistant.getFeatures();
+
+        Iterator<Map.Entry<String, Map<String, String>>> it = features.entrySet().iterator();
+        String next = "";
+        Intent intent = new Intent();
+        while (it.hasNext()) {
+            Map.Entry<String, Map<String, String>> f = (Map.Entry<String, Map<String, String>>) it.next();
+            if (f.getKey().equals("dice")) {
+                if (it.hasNext()) {
+                    Map.Entry<String, Map<String, String>> n = it.next();
+                    next = n.getKey();
+//                    Log.i("dicesettingsact",next);
+                    if(next.equals("table")){
+                        intent = new Intent(this, TableSettingsActivity.class);
+                    }else if(next.equals("timer")) {
+                        intent = new Intent(this, TimerSettingsActivity.class);
+                    }
+                }else{
+                    //TODO:to player settings..
+                }
+                //get settings from spinners and write into assistant
+                String type = sprType.getSelectedItem().toString();
+                String number = sprNumber.getSelectedItem().toString();
+                Map<String, String> settings = assistant.getDice();
+                settings.put("type", type);
+                settings.put("number", number);
+                assistant.setDice(settings);
+                //intent
+                intent.putExtra("assistant",assistant);
+                startActivity(intent);
+            }
+        }
     }
 }
